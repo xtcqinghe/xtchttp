6722fdcce06c2f1088156572fe7ab77dc88b2da2
xtcqinghe <250585183@qq.com>
2026-02-07 14:40:44 +0800

    fix a little problem
    fix a little problem

---
+++ xtchttp/api/domain.py
@@ -0,0 +1,8 @@
+"""
+API域名
+"""
+
+WATCH = "https://watch.okii.com"
+API_WATCH = "https://api.watch.okii.com"
+MOMENT = "https://moment.watch.okii.com"
+SPORT = "https://sport.watch.okii.com"
---
+++ xtchttp/api/path.py
@@ -0,0 +1,41 @@
+"""
+API Path
+"""
+
+# WATCH
+BIND_NUMBER = "/watchaccount/bindnumber"
+FRIEND_INFO = "/watchfriend/watchinfo/int4Boolean"
+FRIEND_ADD = "/watchfriend/group/audit"
+RSA = "/rsa"
+CREATE_GROUP = "/interphone/create"
+UPDATE_NICK_NAME = "/smartwatch/watchaccount/updateBabyName"
+UPDATE_REAL_NAME = "/smartwatch/watchaccount/updateRealName"
+REMARK = "/smartwatch/watchfriend/updateFriendInfoByWatch"
+OAUTH_AUTHORIZE = "/oauth/client/authorizeForWatch"
+
+# API_WATCH
+PERSONAL_INFO = "/social-service/personalInfo/getPersonalInfoUnite"
+PERSONAL_INFO_GET = "/social-service/personalInfo/getPersonalInfoUnite"
+PERSONAL_INFO_LIKE = "/social-service/personalInfo/like"
+PERSONAL_INFO_BIU = "/social-service/personalInfo/biu"
+PERSONAL_INFO_LIKE_DETAIL = "/social-service/personalInfo/getLikeDetailPage"
+PERSONAL_INFO_UPDATE_SIGNATURE = "/social-service/personalInfo/updateSignature"
+SPEED_CALCULATION_UPLOAD_SCORE = "/watch-game-service/speedCalculation/uploadScore"
+SPORT_UPLOAD_RECORD = "/sport-service/sport/i25/uploadWatchSportRecord"
+EQUITY_CARD_BUY_CHANCE = "/social-service/prerogative/buychance"
+
+# MOMENT
+PUBLISH = "/moment/public"
+MOMENT_LIKE = "/moment/like"
+MOMENT_LIKE_SEARCH = "/moment/like/search"
+MOMENT_DELETE = "/moment/delete"
+MOMENT_SEARCH = "/moment/search"
+FILE_PIC_TRANSFER = "/moment/file/pic/transfer"
+FILE_VIDEO_TRANSFER = "/moment/file/video/transfer"
+FILE_BATCH_DOWNLOAD = "/moment/file/batchDownload"
+
+# SPORT
+STEP_UPLOAD = "/sportweather/sport/i6"
+
+
+
